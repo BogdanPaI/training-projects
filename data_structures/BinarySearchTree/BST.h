94efe242f7bb2740fb31402998bab5d6b5b25a65
Bogdan <bogdan7937@gmail.com>
2019-02-25 14:38:23 +0300

    Update BST.h
    Added Insert method and corrected mistakes.

--- data_structures/BinarySearchTree/BST.h
+++ data_structures/BinarySearchTree/BST.h
@@ -26,30 +26,62 @@ namespace data_struct {
             ~BST() {}
             
             // Function that return new node of BST
-            BSTNode* GetNewNode(BSTNode* parent, const K& key, const T& value) {
-                BSTNode *new_node = new BSTNode;
+            BSTNode<K, T>* GetNewNode(BSTNode<K, T>* parent, const K& key, const T& value) {
+                BSTNode<K, T> *new_node = new BSTNode<K, T>;
                 new_node->SetKey(key);
                 new_node->SetValue(value);
                 new_node->SetParent(parent);
                 new_node->SetLeft(nullptr);
-                new_node->Setright(nullptr);
+                new_node->SetRight(nullptr);
                 return new_node;
             }
-
-            BSTNode* Search(const K& key) {
-                BSTNode *temp = root_;
-                while (temp != nullptr && key != temp->GetKey()) {
+            
+            // Searching node in the tree
+            bool Search(const K& key) {
+                BSTNode<K, T> *temp = root_;
+                while (temp != nullptr) {
                     if (key < temp-> GetKey()) {
                         temp = temp->GetLeft();
                     } else if (key > temp->GetKey()) {
                         temp = temp->GetRight();
+                    } else {
+                        return true;
                     }
                 }
-                return temp;
+                return false;
+            }
+            
+            // Inserting new node in the tree
+            void Insert(const K& key, const T& value) {
+				if (root_ == nullptr) {
+					root_ = GetNewNode(root_, key, value);
+					return;
+				}
+
+                BSTNode<K, T> *current = root_;
+                BSTNode<K, T> *previous = root_;
+
+                while (current != nullptr) {
+                    if (key < current-> GetKey()) {
+                        previous = current;
+                        current = current->GetLeft();
+                    } else if (key > current->GetKey()) {
+                        previous = current;
+                        current = current->GetRight();
+                    }
+                }
+
+				if (key < previous->GetKey()) {
+					BSTNode<K, T> *new_node = GetNewNode(previous, key, value);
+					previous->SetLeft(new_node);
+				} else if (key > previous->GetKey()) {
+					BSTNode<K, T> *new_node = GetNewNode(previous, key, value);
+					previous->SetRight(new_node);
+				}
             }
 
         private:
-            BSTNode *root_;
+            BSTNode<K, T> *root_;
             unsigned int height_;
         };
 }
