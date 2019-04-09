c207a927a446485f7fbb17393157d16bb76c5574
CoffeeIsLife <bogdan7937@gmail.com>
2019-04-09 22:18:29 +0300

    Upgrade of Heap data structure

--- data_structures/Heap/Heap.h
+++ data_structures/Heap/Heap.h
@@ -1,22 +1,34 @@
 #ifndef HEAP_STRUCT_H
 #define HEAP_STRUCT_H
 
+#include <stdexcept>
+
 namespace data_struct {
     template<typename T>
         class Heap {
         public:
             // Default constructor
-            Heap(size_t size);
+            Heap(size_t capacity) {
+                size_ = 0;
+                capacity_ = capacity;
+                data_ = new T[capacity_];
+            }
             // Destructor
-            ~Heap();
+            ~Heap() { delete [] data_; }
             // Function that returns size of heap
-            size_t GetSize() const;
+            size_t GetSize() const { return size_; }
             // Function that checks emptiness of heap
-            bool IsEmpty() const;
+            bool IsEmpty() const { return size_ == 0; }
             // Function that inserts element at the end of array
-            void Insert(const T& item);
+            void Insert(const T& item) {
+                if ( size_ < capacity_) {
+                    data_[++size_] = item;
+                } else {
+                    throw std::overflow_error("Heap is full.")                
+                }
+            };
             // Function that returns the max item
-            T& GetMax();
+            T& GetMax() { return data_[1]; }
         private:
             // The upword restoration of pyramidal structure of heap(need for insertion)
             void Up();
@@ -25,6 +37,7 @@ namespace data_struct {
         private:
             T *data_;
             size_t size_;
+            size_t capacity_;
         };
 }
 #endif
