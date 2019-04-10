d8f5ec7cf4fbc23fe560ccf5416bec431c44a24d
CoffeeIsLife <bogdan7937@gmail.com>
2019-04-10 20:13:02 +0300

    Down function implemented.

--- data_structures/Heap/Heap.h
+++ data_structures/Heap/Heap.h
@@ -15,7 +15,7 @@ namespace data_struct {
             }
 
             // Destructor
-            ~Heap() { delete [] data_; }
+            ~Heap() { delete[] data_; }
 
             // Function that returns size of heap
             size_t GetSize() const { return size_; }
@@ -25,7 +25,7 @@ namespace data_struct {
 
             // Function that inserts element at the end of array
             void Insert(const T& item) {
-                if ( size_ < capacity_) {
+                if ( size_ < capacity_ ) {
                     data_[++size_] = item;
                     Up(size_);
                 } else {
@@ -34,21 +34,42 @@ namespace data_struct {
             }
 
             // Function that returns the max item
-            T& GetMax() { return data_[1]; }
+            T& GetMax() { 
+                T max = data_[1];
+                data_[1] = data_[size_];
+                data_[size_] = NULL;
+                size_--;
+                Down(1);
+                return max;
+            }
 
         private:
-            // The upword restoration of pyramidal structure of heap(need for insertion)
+            // The upword heapifying(need for insertion)
             void Up(size_t i) {
-                while ( i > 1 && data_[i / 2] < data_[i]) {
+                while ( i > 1 && data_[i / 2] < data_[i] ) {
                     T swap = data_[i / 2];
                     data_[i / 2] = data_[i];
                     data_[i] = swap;
                     i = i / 2;
                 }
-            };
+            }
 
-            // Descending restoration of pyramidal structure of heap(nead for GetMax function)
-            void Down();
+            // Descending heapifying(nead for GetMax function)
+            void Down(size_t i) {
+                while ( 2*i <= size_) {
+                    size_t j = 2*i;
+                    if (j < size_ && data_[j] < data_[j + 1]) {
+                        j++;
+                    }
+                    if ( !(data_[i] < data_[j]) ) {
+                        break;
+                    }
+                    T swap = data_[j];
+                    data_[j] = data_[i];
+                    data_[i] = swap;
+                    i = j;
+                }
+            }
 
         private:
             T *data_;
